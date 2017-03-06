81e4d4b2d79e09008b6828ca352714a5b5bc824f
Knut Ahlers <knut@ahlers.me>
2017-03-06 21:59:53 +0100

    Move builders to own file
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"archive/tar"
-	"archive/zip"
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -13,12 +10,9 @@ import (
 	"os"
 	"os/exec"
 	"path"
-	"path/filepath"
 	"strconv"
-	"strings"
 	"time"
 
-	"github.com/Luzifer/go_helpers/str"
 	"github.com/Luzifer/rconfig"
 	log "github.com/Sirupsen/logrus"
 	"github.com/gorilla/mux"
@@ -232,95 +226,6 @@ func waitForJob(res http.ResponseWriter, r *http.Request) {
 		u := urlMust(router.Get("downloadAssets").URL("uid", uid.String()))
 		http.Redirect(res, r, u.String(), http.StatusFound)
 	}
-}
-
-func shouldPackFile(extension string) bool {
-	return str.StringInSlice(extension, []string{
-		".log",
-		".pdf",
-	})
-}
-
-func buildAssetsZIP(uid uuid.UUID) (io.Reader, error) {
-	buf := new(bytes.Buffer)
-	w := zip.NewWriter(buf)
-
-	basePath := pathFromUUID(uid, filenameOutputDir)
-	err := filepath.Walk(basePath, func(p string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if !shouldPackFile(path.Ext(info.Name())) {
-			return nil
-		}
-
-		zipInfo, err := zip.FileInfoHeader(info)
-		if err != nil {
-			return err
-		}
-		zipInfo.Name = strings.TrimLeft(strings.Replace(p, basePath, "", 1), "/\\")
-		zipFile, err := w.CreateHeader(zipInfo)
-		if err != nil {
-			return err
-		}
-		osFile, err := os.Open(p)
-		if err != nil {
-			return err
-		}
-
-		io.Copy(zipFile, osFile)
-		osFile.Close()
-
-		return nil
-	})
-
-	if err != nil {
-		return nil, err
-	}
-
-	return buf, w.Close()
-}
-
-func buildAssetsTAR(uid uuid.UUID) (io.Reader, error) {
-	buf := new(bytes.Buffer)
-	w := tar.NewWriter(buf)
-
-	basePath := pathFromUUID(uid, filenameOutputDir)
-	err := filepath.Walk(basePath, func(p string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if !shouldPackFile(path.Ext(info.Name())) {
-			return nil
-		}
-
-		tarInfo, err := tar.FileInfoHeader(info, "")
-		if err != nil {
-			return err
-		}
-		tarInfo.Name = strings.TrimLeft(strings.Replace(p, basePath, "", 1), "/\\")
-		err = w.WriteHeader(tarInfo)
-		if err != nil {
-			return err
-		}
-		osFile, err := os.Open(p)
-		if err != nil {
-			return err
-		}
-
-		io.Copy(w, osFile)
-		osFile.Close()
-
-		return nil
-	})
-
-	if err != nil {
-		return nil, err
-	}
-
-	return buf, w.Close()
 }
 
 func downloadAssets(res http.ResponseWriter, r *http.Request) {
